525b0bbb70317cc23c8131a24966f79fd687cb4d
Noah- Yannick Schmid <44346907+nonoyona@users.noreply.github.com>
2024-04-27 01:07:27 +0300

    Update heat_radius calculation in PeopleMovementHeatmap class

--- people_move_heatmap.py
+++ people_move_heatmap.py
@@ -27,7 +27,7 @@ class PeopleMovementHeatmap:
             self.device = "cpu"
 
         self.heat_per_person = 10
-        self.heat_radius = 100 * scale
+        self.heat_radius = int(100 * scale)
         self.heat_per_distance = 2
         self.heat_decay = 0.9
         self.model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50").to(self.device)
