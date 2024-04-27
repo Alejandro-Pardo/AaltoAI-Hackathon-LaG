0ce38d252bf6cdea10b2f6e39ff346958f3fb1fb
Leon Magnus <leon.magnus@aalto.fi>
2024-04-27 03:28:04 +0300

    Added data creation pipeline and encoder decoder models

---
+++ Create_Data.py
@@ -0,0 +1,18 @@
+from file_camera import FileCamera
+from MakFormer import MaskFormer
+import numpy as np
+cam = FileCamera("IMG_1081.mp4",predefined_size=(1280, 720))
+maskformer = MaskFormer(cam.get_shape())
+current_frame = cam.get_image_one_by_one()
+counter = 0
+while current_frame is not None:
+    data = np.zeros((1280,720,2))
+    current_frame = cam.get_image_one_by_one()
+    print(current_frame.shape)
+    heat_map = maskformer.gen_heat(current_frame)
+    ang = maskformer.ang
+    data[:,:,0] = heat_map.T
+    if ang is not None:
+        data[:,:,1] = ang.T
+    np.save(f'data/hack/data_{counter}.npy', data)
+    counter += 1
---
+++ predict_heatmap.py
@@ -0,0 +1,4 @@
+import torch
+import numpy as np
+from MakFormer import MaskFormer
+from file_camera import FileCamera
