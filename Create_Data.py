fe6733a4612b9496bd3491dba56fab82e6578e50
Leon Magnus <leon.magnus@aalto.fi>
2024-04-27 03:51:08 +0300

    Adjusted Create data

--- Create_Data.py
+++ Create_Data.py
@@ -3,7 +3,7 @@ from MakFormer import MaskFormer
 import numpy as np
 import torch
 device = "cuda" if torch.cuda.is_available() else "cpu"
-cam = FileCamera("IMG_1081.mp4",predefined_size=(1280, 720))
+cam = FileCamera("/root/lisan-ai-gaib/AaltoAI-Hackathon-LaG/IMG_1081.mp4",predefined_size=(1280, 720))
 maskformer = MaskFormer(cam.get_shape(),device=device)
 current_frame = cam.get_image_one_by_one()
 counter = 0
@@ -16,5 +16,5 @@ while current_frame is not None:
     data[:,:,0] = heat_map.T
     if ang is not None:
         data[:,:,1] = ang.T
-    np.save(f'data/hack/data_{counter}.npy', data)
+    np.save(f'/root/lisan-ai-gaib/AaltoAI-Hackathon-LaG/data/hack/data_{counter}.npy', data)
     counter += 1
